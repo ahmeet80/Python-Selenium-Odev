2840405a10945c03186295c97cb62bb6e8e9b415
Ahmet Karaaslan <ahmeet80@gmail.com>
2023-03-15 19:17:54 +0300

    15.03.2023_19:17

--- odev01.py
+++ odev01.py
@@ -1,15 +1,8 @@
-# Sayılar (Numbers): Python'da üç farklı sayı tipi bulunur: tam sayılar (int), kesirli sayılar(float) ve karmaşık sayılar (complex).
-
-
-# Diziler (Lists): Listenin her bir elemanı virgülle ayrılacak şekilde tanımlanır ve köşeli parantez içinde yazılır. Dizilerde farklı veri tipleri bir arada kullanılabilir. Java'daki Array Listler gibi.
-
-# Demetler (Tuples): Demetler, elemanları değiştirilemeyen bir listtir. Demetler, normal parantez içinde virgülle ayrılmış elemanlarla tanımlanır. Javadaki Arraylere benzerler.
-
-# Sözlükler (Dictionaries): Sözlükler, key-value çiftleri kullanarak elemanları depolayan bir veri tipidir. Sözlükler süslü parantez içinde tanımlanır. Java'daki MAP ile benzerdir.
-
-# Kümeler (Sets): Kümeler, tekil elemanlardan oluşan bir veri tipidir. Kümeler süslü parantez içinde tanımlanır. Yani tekrarsız bir Array List gibi düşünülebilir.
-
-
-# Karakter Dizileri (Strings): Karakter dizileri, bir veya daha fazla karakterden oluşan bir veri tipidir. Karakter dizileri tırnak işaretleri arasında yazılır.
-
-# Boolean (Mantıksal Değerler): Boolean, yalnızca True (doğru) veya False (yanlış) olabilen bir veri tipidir.
+tamamlanan = 78
+if tamamlanan < 50:
+    print("Kurs tamamlanmadi")
+elif tamamlanan == 50:
+    print("Kursun yarisi tamamlandi")
+elif tamamlanan < 100:
+    print("Kurs tamamlanmak uzere")
+else: print("Kurs tamamlandi")
